af1ed6d07aaa525f6ee6bfa9bcc842e3d751ec08
horsicq <horsicq@gmail.com>
2021-10-26 22:38:39 +0200

    Fix: 2021-10-26

--- dialoggotoaddress.cmake
+++ dialoggotoaddress.cmake
@@ -1,1 +1,9 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
+
+include(${CMAKE_CURRENT_LIST_DIR}/../Controls/xlineedithex.cmake)
+
+set(DIALOGGOTOADDRESS_SOURCES
+    ${XLINEEDITHEX_SOURCES}
+    ${CMAKE_CURRENT_LIST_DIR}/dialoggotoaddress.ui
+    ${CMAKE_CURRENT_LIST_DIR}/dialoggotoaddress.cpp
+    )
--- dialoggotoaddress.pri
+++ dialoggotoaddress.pri
@@ -14,3 +14,6 @@ SOURCES += \
     XCONFIG += xlineedithex
     include($$PWD/../Controls/xlineedithex.pri)
 }
+
+DISTFILES += \
+    $$PWD/dialoggotoaddress.cmake
