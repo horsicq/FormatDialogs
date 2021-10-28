52cce89dadd1c2fd09ba57688a552b788df811ec
horsicq <horsicq@gmail.com>
2021-10-28 08:43:14 +0200

    Fix: 2021-10-28

--- dialogdump.cmake
+++ dialogdump.cmake
@@ -3,4 +3,5 @@ include_directories(${CMAKE_CURRENT_LIST_DIR})
 set(DIALOGDUMP_SOURCES
     ${CMAKE_CURRENT_LIST_DIR}/dialogdumpprocess.cpp
     ${CMAKE_CURRENT_LIST_DIR}/dialogdumpprocess.ui
+    ${CMAKE_CURRENT_LIST_DIR}/dumpprocess.cpp
     )
