feb8ecc1bf741e16f28c444b084dea4408473057
horsicq <horsicq@gmail.com>
2022-02-04 18:46:34 +0100

    Fix: 2022-02-04

--- dialogdumpprocess.cpp
+++ dialogdumpprocess.cpp
@@ -21,7 +21,7 @@
 #include "dialogdumpprocess.h"
 #include "ui_dialogdumpprocess.h"
 
-DialogDumpProcess::DialogDumpProcess(QWidget *pParent, QIODevice *pDevice, qint64 nOffset, qint64 nSize, QString sFileName, DumpProcess::DT dumpType) :
+DialogDumpProcess::DialogDumpProcess(QWidget *pParent,QIODevice *pDevice,qint64 nOffset,qint64 nSize,QString sFileName,DumpProcess::DT dumpType) :
     QDialog(pParent),
     ui(new Ui::DialogDumpProcess)
 {
