d12df2c28fa1221215993a08838563bcfdff3018
horsicq <horsicq@gmail.com>
2022-02-05 16:06:50 +0100

    Fix: 2022-02-05

--- dialoggotoaddress.cpp
+++ dialoggotoaddress.cpp
@@ -21,7 +21,7 @@
 #include "dialoggotoaddress.h"
 #include "ui_dialoggotoaddress.h"
 
-DialogGoToAddress::DialogGoToAddress(QWidget *pParent, XBinary::_MEMORY_MAP *pMemoryMap, TYPE type) :
+DialogGoToAddress::DialogGoToAddress(QWidget *pParent,XBinary::_MEMORY_MAP *pMemoryMap,TYPE type) :
     XShortcutsDialog(pParent),
     ui(new Ui::DialogGoToAddress)
 {
