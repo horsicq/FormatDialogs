16ba9a2b01a280f9aa242722646239f6343cd625
horsicq <horsicq@gmail.com>
2022-05-14 20:58:35 +0200

    Fix: 2022-05-14

--- dialogsearch.cpp
+++ dialogsearch.cpp
@@ -21,7 +21,7 @@
 #include "dialogsearch.h"
 #include "ui_dialogsearch.h"
 
-DialogSearch::DialogSearch(QWidget *pParent, QIODevice *pDevice, SearchProcess::SEARCHDATA *pSearchData, SEARCHMODE searchMode) :
+DialogSearch::DialogSearch(QWidget *pParent,QIODevice *pDevice,SearchProcess::SEARCHDATA *pSearchData,SEARCHMODE searchMode) :
     QDialog(pParent),
     ui(new Ui::DialogSearch)
 {
