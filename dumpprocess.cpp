146b123dc482faecd38b91dcedd0d59069a53123
horsicq <horsicq@gmail.com>
2022-07-02 00:12:10 +0200

    Fix: 2022-07-02

--- dumpprocess.cpp
+++ dumpprocess.cpp
@@ -25,7 +25,7 @@ DumpProcess::DumpProcess(QObject *pParent) : QObject(pParent)
     g_pPdStruct=nullptr;
 }
 
-void DumpProcess::setData(QIODevice *pDevice, qint64 nOffset, qint64 nSize, QString sFileName, DT dumpType, XBinary::PDSTRUCT *pPdStruct)
+void DumpProcess::setData(QIODevice *pDevice,qint64 nOffset,qint64 nSize,QString sFileName,DT dumpType,XBinary::PDSTRUCT *pPdStruct)
 {
     this->g_pDevice=pDevice;
     this->g_nOffset=nOffset;
