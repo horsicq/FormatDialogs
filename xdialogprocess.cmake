293ea02a363cc434c7d3f2cc99372b9b1e74070a
horsicq <horsicq@gmail.com>
2024-03-09 01:58:31 +0100

    Update file(s): 2024-03-09

--- xdialogprocess.cmake
+++ xdialogprocess.cmake
@@ -2,5 +2,6 @@ include_directories(${CMAKE_CURRENT_LIST_DIR})
 
 set(XDIALOGPROCESS_SOURCES
     ${CMAKE_CURRENT_LIST_DIR}/xdialogprocess.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xdialogprocess.h
     ${CMAKE_CURRENT_LIST_DIR}/xdialogprocess.ui
     )
