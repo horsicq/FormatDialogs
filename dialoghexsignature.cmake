8fd90ff62c4f84a48bbf7616199e2b4dc7926506
horsicq <horsicq@gmail.com>
2024-05-09 03:13:25 +0200

    Update file(s): 2024-05-09

--- dialoghexsignature.cmake
+++ dialoghexsignature.cmake
@@ -5,5 +5,6 @@ include(${CMAKE_CURRENT_LIST_DIR}/../FormatWidgets/SearchValues/searchvalueswidg
 set(DIALOGHEXSIGNATURE_SOURCES
     ${SEARCHVALUESWIDGET_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/dialoghexsignature.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialoghexsignature.h
     ${CMAKE_CURRENT_LIST_DIR}/dialoghexsignature.ui
     )
