3948f24e28296f9e58d6f34bf26b70907fc8d6c3
horsicq <horsicq@gmail.com>
2024-05-12 02:25:15 +0200

    Update file(s): 2024-05-12

--- dialogtextinfo.cmake
+++ dialogtextinfo.cmake
@@ -4,5 +4,6 @@ include_directories(${CMAKE_CURRENT_LIST_DIR})
 
 set(DIALOGTEXTINFO_SOURCES
     ${CMAKE_CURRENT_LIST_DIR}/dialogtextinfo.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogtextinfo.h
     ${CMAKE_CURRENT_LIST_DIR}/dialogtextinfo.ui
     )
