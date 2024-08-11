c5b9655d4dad74ad1a7ea1fbb0e96729ac97668d
horsicq <horsicq@gmail.com>
2024-08-12 00:32:22 +0200

    Update file(s): 2024-08-12

--- dumpprocess.h
+++ dumpprocess.h
@@ -57,7 +57,7 @@ public:
     void setData(QIODevice *pDevice, QList<RECORD> listRecords, DT dumpType, const QString &sJsonFileName, XBinary::PDSTRUCT *pPdStruct);
     void setData(QIODevice *pDevice, DT dumpType, const QString &sJsonFileName, XBinary::PDSTRUCT *pPdStruct);
 #ifdef USE_XPROCESS
-    void setData(X_ID nProcessID, XADDR nAddress, qint64 nSize, DT dumpType, QString sFileName, QString sJsonFileName, XBinary::PDSTRUCT *pPdStruct);
+    void setData(X_ID nProcessID, XADDR nAddress, qint64 nSize, DT dumpType, const QString &sFileName, const QString &sJsonFileName, XBinary::PDSTRUCT *pPdStruct);
 #ifdef Q_OS_WIN
     void setData(X_ID nProcessID, XADDR nAddress, qint64 nSize, DT dumpType, QString sFileName, QString sJsonFileName, const XPE::FIXDUMP_OPTIONS &fixDumpOptions,
                  const QByteArray &baHeaders, XBinary::PDSTRUCT *pPdStruct);
