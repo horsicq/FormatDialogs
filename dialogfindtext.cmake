e0f13f27a46043236c69ce12ab0e19efc90bd74e
horsicq <horsicq@gmail.com>
2024-09-05 00:13:32 +0200

    Update file(s): 2024-09-05

--- dialogfindtext.cmake
+++ dialogfindtext.cmake
@@ -1,7 +1,5 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
-#TODO
-
 set(DIALOGFINDTEXT_SOURCES
     ${CMAKE_CURRENT_LIST_DIR}/dialogfindtext.cpp
     ${CMAKE_CURRENT_LIST_DIR}/dialogfindtext.h
