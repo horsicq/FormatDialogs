9c6b69e280ee3ae48814146d494462df10968f2d
horsicq <horsicq@gmail.com>
2024-11-27 18:48:51 +0100

    Add new file(s): 2024-11-27

---
+++ dialogwidget.cmake
@@ -0,0 +1,9 @@
+include_directories(${CMAKE_CURRENT_LIST_DIR})
+
+#TODO
+
+set(DIALOGWIDGET_SOURCES
+    ${CMAKE_CURRENT_LIST_DIR}/dialogwidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogwidget.h
+    ${CMAKE_CURRENT_LIST_DIR}/dialogwidget.ui
+    )
---
+++ dialogwidget.pri
@@ -0,0 +1,11 @@
+INCLUDEPATH += $$PWD
+DEPENDPATH += $$PWD
+
+HEADERS += \
+    $$PWD/dialogwidget.h
+
+SOURCES += \
+    $$PWD/dialogwidget.cpp
+
+FORMS += \
+    $$PWD/dialogwidget.ui
