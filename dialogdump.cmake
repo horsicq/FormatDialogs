d4ff750e1719f6fc0ee46795306278e05321777c
horsicq <horsicq@gmail.com>
2024-11-30 11:17:15 +0100

    Update file(s): 2024-11-30

--- dialogdump.cmake
+++ dialogdump.cmake
@@ -1,8 +1,12 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
-include(${CMAKE_CURRENT_LIST_DIR}/xdialogprocess.cmake)
+if (NOT DEFINED XDIALOGPROCESS_SOURCES)
+    include(${CMAKE_CURRENT_LIST_DIR}/../FormatDialogs/xdialogprocess.cmake)
+    set(DIALOGDUMP_SOURCES ${DIALOGDUMP_SOURCES} ${XDIALOGPROCESS_SOURCES})
+endif()
 
 set(DIALOGDUMP_SOURCES
+    ${DIALOGDUMP_SOURCES}
     ${XDIALOGPROCESS_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/dialogdumpprocess.cpp
     ${CMAKE_CURRENT_LIST_DIR}/dialogdumpprocess.h
