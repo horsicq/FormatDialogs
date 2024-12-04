23e38b5929023d7af6e59ad9b19e2efa8bc6de39
horsicq <horsicq@gmail.com>
2024-12-04 08:38:22 +0100

    Update file(s): 2024-12-04

--- dialoghexsignature.cmake
+++ dialoghexsignature.cmake
@@ -1,8 +1,12 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
-include(${CMAKE_CURRENT_LIST_DIR}/../FormatWidgets/SearchValues/searchvalueswidget.cmake)
+if (NOT DEFINED SEARCHVALUESWIDGET_SOURCES)
+    include(${CMAKE_CURRENT_LIST_DIR}/../FormatWidgets/SearchValues/searchvalueswidget.cmake)
+    set(DIALOGHEXSIGNATURE_SOURCES ${DIALOGHEXSIGNATURE_SOURCES} ${SEARCHVALUESWIDGET_SOURCES})
+endif()
 
 set(DIALOGHEXSIGNATURE_SOURCES
+    ${DIALOGHEXSIGNATURE_SOURCES}
     ${SEARCHVALUESWIDGET_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/dialoghexsignature.cpp
     ${CMAKE_CURRENT_LIST_DIR}/dialoghexsignature.h
