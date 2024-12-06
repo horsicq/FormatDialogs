d798515640b423773b4cc6baa6747016fe16ea66
horsicq <horsicq@gmail.com>
2024-12-06 08:35:09 +0100

    Update file(s): 2024-12-06

--- dialogresize.cmake
+++ dialogresize.cmake
@@ -1,8 +1,12 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
-include(${CMAKE_CURRENT_LIST_DIR}/../Controls/xlineedithex.cmake)
+if (NOT DEFINED XLINEEDITHEX_SOURCES)
+    include(${CMAKE_CURRENT_LIST_DIR}/../Controls/xlineedithex.cmake)
+    set(XLINEEDITHEX_SOURCES ${XLINEEDITHEX_SOURCES} ${XLINEEDITHEX_SOURCES})
+endif()
 
 set(DIALOGRESIZE_SOURCES
+    ${DIALOGRESIZE_SOURCES}
     ${XLINEEDITHEX_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/dialogresize.cpp
     ${CMAKE_CURRENT_LIST_DIR}/dialogresize.h
