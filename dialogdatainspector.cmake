1ea90e8dce31e27cdf4bfb30689248e0e05bf55e
horsicq <horsicq@gmail.com>
2024-12-17 00:50:43 +0100

    Update file(s): 2024-12-17

--- dialogdatainspector.cmake
+++ dialogdatainspector.cmake
@@ -1,6 +1,7 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
 set(DIALOGDATAINSPECTOR_SOURCES
+    ${DIALOGDATAINSPECTOR_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/dialogdatainspector.cpp
     ${CMAKE_CURRENT_LIST_DIR}/dialogdatainspector.h
     ${CMAKE_CURRENT_LIST_DIR}/dialogdatainspector.ui
