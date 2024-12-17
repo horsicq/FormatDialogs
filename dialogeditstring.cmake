48a5ee0b854f988d18c4b4e0c266b4e7777e2190
horsicq <horsicq@gmail.com>
2024-12-18 00:03:41 +0100

    Update file(s): 2024-12-18

--- dialogeditstring.cmake
+++ dialogeditstring.cmake
@@ -1,6 +1,7 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
 set(DIALOGEDITSTRING_SOURCES
+    ${DIALOGEDITSTRING_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/dialogeditstring.cpp
     ${CMAKE_CURRENT_LIST_DIR}/dialogeditstring.h
     ${CMAKE_CURRENT_LIST_DIR}/dialogeditstring.ui
