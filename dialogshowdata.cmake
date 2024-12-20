405dfdfdfe897d8af57451c8924ccd7716ffc511
horsicq <horsicq@gmail.com>
2024-12-21 00:53:56 +0100

    Update file(s): 2024-12-21

--- dialogshowdata.cmake
+++ dialogshowdata.cmake
@@ -1,6 +1,7 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
 set(DIALOGSHOWDATA_SOURCES
+    ${DIALOGSHOWDATA_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/dialogshowdata.cpp
     ${CMAKE_CURRENT_LIST_DIR}/dialogshowdata.h
     ${CMAKE_CURRENT_LIST_DIR}/dialogshowdata.ui
