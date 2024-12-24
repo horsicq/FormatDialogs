bd795143d73f291e418690f896de98a06752184c
horsicq <horsicq@gmail.com>
2024-12-25 00:58:54 +0100

    Update file(s): 2024-12-25

--- dialogwidget.cmake
+++ dialogwidget.cmake
@@ -3,6 +3,7 @@ include_directories(${CMAKE_CURRENT_LIST_DIR})
 #TODO
 
 set(DIALOGWIDGET_SOURCES
+    ${DIALOGWIDGET_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/dialogwidget.cpp
     ${CMAKE_CURRENT_LIST_DIR}/dialogwidget.h
     ${CMAKE_CURRENT_LIST_DIR}/dialogwidget.ui
