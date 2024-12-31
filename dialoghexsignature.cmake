f6c59eb746cf273f6a55d01eb736907acb1e6e75
horsicq <horsicq@gmail.com>
2025-01-01 00:03:12 +0100

    Update file(s): 2025-01-01

--- dialoghexsignature.cmake
+++ dialoghexsignature.cmake
@@ -7,7 +7,6 @@ endif()
 
 set(DIALOGHEXSIGNATURE_SOURCES
     ${DIALOGHEXSIGNATURE_SOURCES}
-    ${SEARCHVALUESWIDGET_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/dialoghexsignature.cpp
     ${CMAKE_CURRENT_LIST_DIR}/dialoghexsignature.h
     ${CMAKE_CURRENT_LIST_DIR}/dialoghexsignature.ui
